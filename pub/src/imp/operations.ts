040cc505b45b1e6297fd9d09d3b1089f3bb97a38
corno <corno.astn.a@schraverus.com>
2022-09-02 13:54:59 +0200

    first commit

---
+++ pub/src/imp/operations.ts
@@ -0,0 +1,40 @@
+import * as api from "api-pareto-boolean"
+
+import * as pi from "pareto-core-internals"
+
+export const and: api.And = ($) => {
+    let current = true
+    pi.flatten($).forEach(($) => {
+        current = current && $
+    })
+    return current
+}
+
+export const or: api.Or = ($) => {
+    let current = false
+    pi.flatten($).forEach(($) => {
+        current = current || $
+    })
+    return current
+}
+
+export const not: api.Not = ($) => {
+
+    return !$
+}
+
+export const greaterThan: api.GreaterThan = ($) => {
+    return $.this > $.that
+}
+
+export const smallerThan: api.SmallerThan = ($) => {
+    return $.that < $.that
+}
+
+export const equal: api.Equal = ($) => {
+    return $.that === $.that
+}
+
+export const isZero: api.IsZero = ($) => {
+    return $ === 0
+}
