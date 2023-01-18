e991ef2c1f2098d5cc2230ec225f2fb93664fe4e
corno <corno.astn.a@schraverus.com>
2023-01-18 22:12:04 +0100

    .

--- test/src/modules/main/implementations/getTestSet.p.ts
+++ test/src/modules/main/implementations/getTestSet.p.ts
@@ -1,8 +1,8 @@
 
-import * as pm from 'pareto-core-state'
+import * as ps from 'pareto-core-state'
 import * as pl from 'pareto-core-lib'
 
-import * as test from "lib-pareto-test"
+import * as mtest from "lib-pareto-test"
 
 import * as api from "../api"
 
@@ -10,7 +10,7 @@ import * as pub from "../../../../../pub"
 
 export const $$: api.CgetTestSet = () => {
 
-    const builder = pm.createUnsafeDictionaryBuilder<test.TTestElement>()
+    const builder = ps.createUnsafeDictionaryBuilder<mtest.TTestElement>()
     function createTest(name: string, actual: boolean, expect: boolean) {
         builder.add(name, {
             type: ['test', {
