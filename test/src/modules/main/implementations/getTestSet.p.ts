35c2177d72e42043ab5fed1792e30ed6b8b24c5e
corno <corno.astn.a@schraverus.com>
2023-02-23 18:20:40 +0100

    .

--- test/src/modules/main/implementations/getTestSet.p.ts
+++ test/src/modules/main/implementations/getTestSet.p.ts
@@ -1,14 +1,12 @@
 
 import * as ps from 'pareto-core-state'
-import * as pl from 'pareto-core-lib'
+import * as pa from 'pareto-core-async'
 
 import * as mtest from "lib-pareto-test"
+import * as mapi from "../api"
+import * as mpub from "../../../../../pub"
 
-import * as api from "../api"
-
-import * as pub from "../../../../../pub"
-
-export const $$: api.CgetTestSet = () => {
+export const $$: mapi.CgetTestSet = () => {
 
     const builder = ps.createUnsafeDictionaryBuilder<mtest.T.TestElement>()
     function createTest(name: string, actual: boolean, expect: boolean) {
@@ -19,31 +17,31 @@ export const $$: api.CgetTestSet = () => {
         })
     }
 
-    createTest("not true", pub.$a.not(true), false)
-    createTest("not false", pub.$a.not(false), true)
+    createTest("not true", mpub.$a.not(true), false)
+    createTest("not false", mpub.$a.not(false), true)
 
-    createTest("and - empty", pub.$a.and([]), true)
-    createTest("and - false", pub.$a.and([false]), false)
-    createTest("and - trues", pub.$a.and([true, true]), true)
+    createTest("and - empty", mpub.$a.and([]), true)
+    createTest("and - false", mpub.$a.and([false]), false)
+    createTest("and - trues", mpub.$a.and([true, true]), true)
 
-    createTest("or - empty", pub.$a.or([]), false)
-    createTest("or - falses", pub.$a.or([false, false]), false)
-    createTest("or - true", pub.$a.or([true]), true)
+    createTest("or - empty", mpub.$a.or([]), false)
+    createTest("or - falses", mpub.$a.or([false, false]), false)
+    createTest("or - true", mpub.$a.or([true]), true)
 
-    createTest("isZero - not", pub.$a.isZero(1), false)
-    createTest("isZero - yes", pub.$a.isZero(0), true)
+    createTest("isZero - not", mpub.$a.isZero(1), false)
+    createTest("isZero - yes", mpub.$a.isZero(0), true)
 
 
-    createTest("equal - yes", pub.$a.equal({ this: 42, that: 42 }), true)
-    createTest("equal - not", pub.$a.equal({ this: 42, that: 5 }), false)
+    createTest("equal - yes", mpub.$a.equal({ this: 42, that: 42 }), true)
+    createTest("equal - not", mpub.$a.equal({ this: 42, that: 5 }), false)
 
-    createTest("smallerThan - not", pub.$a.smallerThan({ this: 42, that: 42 }), false)
-    createTest("smallerThan - yes", pub.$a.smallerThan({ this: 41, that: 42 }), true)
+    createTest("smallerThan - not", mpub.$a.smallerThan({ this: 42, that: 42 }), false)
+    createTest("smallerThan - yes", mpub.$a.smallerThan({ this: 41, that: 42 }), true)
 
-    createTest("greaterThan - not", pub.$a.greaterThan({ this: 42, that: 42 }), false)
-    createTest("greaterThan - yes", pub.$a.greaterThan({ this: 43, that: 42 }), true)
+    createTest("greaterThan - not", mpub.$a.greaterThan({ this: 42, that: 42 }), false)
+    createTest("greaterThan - yes", mpub.$a.greaterThan({ this: 43, that: 42 }), true)
 
-    return pl.asyncValue({
+    return pa.asyncValue({
         elements: builder.getDictionary()
     })
 }
