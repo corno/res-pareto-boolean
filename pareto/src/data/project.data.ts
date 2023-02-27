17542f4f28b7fb1bccb6c3f2b104e3620cc28774
corno <corno.astn.a@schraverus.com>
2023-02-27 15:10:02 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -9,7 +9,7 @@ import { $ as api } from "./api.data"
 export const $: gproject.T.Project<pd.SourceLocation> = {
     'author': "Corno",
     'description': "a pareto wrapper boolean operations as Pareto lacks them",
-    'license': "ISC",
+    'license': "TBD",
 
     'dependencies': d({
         "glo-pareto-common": {},
