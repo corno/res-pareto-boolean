374a6a55c621fec2ab24901b8117eb302dfd5f08
Corno Schraverus <corno@schraverus.com>
2024-09-03 18:27:12 +0200

    .

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -2,5 +2,5 @@
 
 paretoBuildEnvDir=`realpath $(dirname "$0")`
 
-npm install && \
+npm install pareto-buildenvironment --prefix "$paretoBuildEnvDir" && \
 "$paretoBuildEnvDir/node_modules/pareto-buildenvironment/initializeProject.sh" "$paretoBuildEnvDir/.."
